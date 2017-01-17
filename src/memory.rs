65ce4c0a1a2dfd9073386c9fb5d28ce387879c12
Jakub Podeszwik <jakubpodeszwik@gmail.com>
2017-01-17 23:45:54 +0100

    Simplified reading memory stats

--- src/memory.rs
+++ src/memory.rs
@@ -4,28 +4,23 @@ use std::ops::Deref;
 
 static MEMORY_FILE: &'static str = "/proc/meminfo";
 
-fn str_to_string(s: &str) -> String {
-    s.to_string()
-}
 
 pub fn read_usage() -> Result<String, String> {
     let content = helpers::read_file_to_string(MEMORY_FILE);
     match content {
         Err(err) => Err(err),
         Ok(s) => {
-            let lines: Vec<String> = s.split('\n').map(str_to_string).collect();
-            let mut stats: HashMap<String, String> = HashMap::new();
+            let lines: Vec<&str> = s.split('\n').collect();
+            let mut stats: HashMap<&str, &str> = HashMap::new();
             for line in lines {
-                let parts: Vec<String> = line.split_whitespace().map(str_to_string).collect();
+                let parts: Vec<&str> = line.split(':').map(|s| s.trim()).collect();
+                let parts = parts.deref();
                 if parts.len() >= 2 {
-                    let parts_deref = parts.deref();
-                    let first: String = parts_deref[0].clone();
-                    let second: String = parts_deref[1].clone();
-                    stats.insert(first, second);
+                    stats.insert(parts[0], parts[1]);
                 }
             }
 
-            Ok((*stats.get(&"MemTotal:".to_string()).unwrap()).clone())
+            Ok((*stats.get(&"MemTotal").unwrap()).to_string())
         }
     }
 }
